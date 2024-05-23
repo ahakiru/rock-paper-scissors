f51560e6757ebfeb02d7edf54fdabf6f05c382d0
ahakiru <your.maoka@googlemail.com>
2024-05-23 21:35:47 +0200

    Add getComputerchoice function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let number = Math.random()*3
+    if (number >= 2) {return "Rock"}
+    else if (number >= 1) {return "Paper"}
+    else {return "Scissors"};
+}
+console.log(getComputerChoice())
