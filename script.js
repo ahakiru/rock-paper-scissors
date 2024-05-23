1bb8dbcb78461bc6dfec23fc07f80fc2f35aa803
ahakiru <your.maoka@googlemail.com>
2024-05-23 21:50:32 +0200

    Add getHumanChoice function.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function getComputerChoice() {
     let number = Math.random()*3
-    if (number >= 2) {return "Rock"}
-    else if (number >= 1) {return "Paper"}
-    else {return "Scissors"};
+    if (number >= 2) {return "rock"}
+    else if (number >= 1) {return "paper"}
+    else {return "scissors"};
 }
+
+function  getHumanChoice () {
+    let choice = prompt("Rock, paper or scissors?");
+    if (choice.toLowerCase() == "rock") {return "rock"}
+    else if (choice.toLowerCase() == "paper") {return "paper"}
+    else if (choice.toLowerCase() == "scissors") {return "scissors"}
+    else {console.log("Error,input not found, try again.")}
+    }
 console.log(getComputerChoice())
+console.log(getHumanChoice())
